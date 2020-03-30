946c8f5a66efa93a5b3d045e78c650ef029fea98
DedushkaDed <50992188+DedushkaDed@users.noreply.github.com>
2020-03-30 17:45:39 +0500

    Initial commit

---
+++ app/src/main/java/glebkalinin/test/instagramclone/MainActivity.kt
@@ -0,0 +1,60 @@
+package glebkalinin.test.instagramclone
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import glebkalinin.test.instagramclone.Fragments.HomeFragment
+import glebkalinin.test.instagramclone.Fragments.NotificationsFragment
+import glebkalinin.test.instagramclone.Fragments.ProfileFragment
+import glebkalinin.test.instagramclone.Fragments.SearchFragment
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    internal var selectedFragment: Fragment? = null
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                selectedFragment = HomeFragment()
+            }
+            R.id.nav_search -> {
+                selectedFragment = SearchFragment()
+            }
+            R.id.nav_add_post -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                selectedFragment = NotificationsFragment()
+            }
+            R.id.nav_profile -> {
+                selectedFragment = ProfileFragment()
+            }
+        }
+
+        if (selectedFragment != null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                selectedFragment!!
+            ).commit()
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            HomeFragment()
+        ).commit()
+    }
+}
