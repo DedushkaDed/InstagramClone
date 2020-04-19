240ceef8e335b4a3e02194ace5e63b2e3a2c39a2
dedushkaded <glebstuck@yandex.ru>
2020-04-19 14:48:19 +0500

    Регистрация, Логин. Профиль, Изменить профиль. 18.04.2020 Меню поиск работает и ищет всех зарег пользователей 19.04.2020 Подписка Отписка на пользователей в меню Поиск

--- app/src/main/java/glebkalinin/test/instagramclone/adapter/UserAdapter.kt
+++ app/src/main/java/glebkalinin/test/instagramclone/adapter/UserAdapter.kt
@@ -9,6 +9,12 @@ import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import glebkalinin.test.instagramclone.Model.User
@@ -18,6 +24,8 @@ class UserAdapter (private var mContext: Context,
                    private var mUser: List<User>,
                    private var isFragment: Boolean = false) : RecyclerView.Adapter<UserAdapter.ViewHolder>()
 {
+    private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
         val view = LayoutInflater.from(mContext).inflate(R.layout.user_item_layout, parent, false)
         return UserAdapter.ViewHolder(view)
@@ -33,7 +41,63 @@ class UserAdapter (private var mContext: Context,
         holder.userNameTextView.text = user.getUsername()
         holder.userFullnameTextView.text = user.getFullname()
         Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(holder.userProfileImage) // Отображение дефолтной картинки.
+
+        checkFollowingStatus(user.getUID(), holder.followButton)
+
+        holder.followButton.setOnClickListener {
+            // Если пользователь(#1) не подписан на (другого пользователя(#2). Пользователь на которого мы подписываемся(#2) - будет добавлен в "Following"-"Подписаны" лист.
+            // А внутри профиля (#2) - Пользователь(#1), будет добавлен в его "Followers"- "Подписчики" лист.
+            if (holder.followButton.text.toString() == "Follow")
+            {
+                firebaseUser?.uid.let { it1 ->
+                    FirebaseDatabase.getInstance().reference // reference to the root. ссылаемся на папочки Firebase
+                        .child("Follow").child(it1.toString()) // it1.toString() == FirebaseUser.get uid() - В джаве.
+                        .child("Following").child(user.getUID())
+                        .setValue(true).addOnCompleteListener { task ->
+                            if (task.isSuccessful)
+                            {
+                                firebaseUser?.uid.let { it1 ->
+                                    FirebaseDatabase.getInstance().reference
+                                        .child("Follow").child(user.getUID())
+                                        .child("Followers").child(it1.toString())
+                                        .setValue(true).addOnCompleteListener { task ->
+                                            if (task.isSuccessful)
+                                            {
+
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                }
+            }
+            else    // Если пользователь уже подписан -> на другого пользователя. И нажимает ещё раз на кнопку (отписывается от пользователя)
+            {
+                firebaseUser?.uid.let { it1 ->
+                    FirebaseDatabase.getInstance().reference
+                        .child("Follow").child(it1.toString())
+                        .child("Following").child(user.getUID())
+                        .removeValue().addOnCompleteListener { task ->          // .removeValue()
+                            if (task.isSuccessful)
+                            {
+                                firebaseUser?.uid.let { it1 ->
+                                    FirebaseDatabase.getInstance().reference
+                                        .child("Follow").child(user.getUID())
+                                        .child("Followers").child(it1.toString())
+                                        .removeValue().addOnCompleteListener { task ->      // .removeValue()
+                                            if (task.isSuccessful)
+                                            {
+
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                }
+            }
+        }
     }
+
 
 
     class ViewHolder (@NonNull itemView: View) : RecyclerView.ViewHolder(itemView){
@@ -41,6 +105,34 @@ class UserAdapter (private var mContext: Context,
         var userFullnameTextView: TextView = itemView.findViewById(R.id.user_fullname_search)
         var userProfileImage: CircleImageView = itemView.findViewById(R.id.user_profile_image_search)
         var followButton: Button = itemView.findViewById(R.id.follow_btn_search)
+    }
+
+
+
+    private fun checkFollowingStatus(uid: String, followButton: Button) {
+        val followingRef = firebaseUser?.uid.let { it1 ->
+            FirebaseDatabase.getInstance().reference // reference to the root. ссылаемся на папочки Firebase
+                .child("Follow").child(it1.toString()) // it1.toString() == FirebaseUser.get uid() - В джаве.
+                .child("Following")
+        }
+
+        followingRef.addValueEventListener(object: ValueEventListener
+        {
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (datasnapshot.child(uid).exists())
+                {
+                    followButton.text = "Following"
+                }
+                else
+                {
+                    followButton.text = "Follow"
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
     }
 
 }
